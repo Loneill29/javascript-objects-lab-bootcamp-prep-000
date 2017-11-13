0e38bd9c7259e76ffcfef7ac5a506a8814731e2e
Linda ONeill <oneill.linda21@gmail.com>
2017-11-13 21:37:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var recipes = {typeOf: "recipe"}
 function destructivelyUpdateObjectWithKeyAndValue(recipes, typeOf, recipe) {recipes[typeOf] = recipe
 return recipes}
 function updateObjectWithKeyAndValue(recipes, typeOf, recipe) {return Object.assign({}, recipes, { [typeOf]: recipe })}
-function destructivelyDeleteFromObjectByKey(recipes, typeOf) { typeOf: recipe;  return delete recipes.typeOf}
+function destructivelyDeleteFromObjectByKey(recipes, typeOf) {delete recipes[typeOf]
+return recipes
+}
 function deleteFromObjectByKey(recipes, typeOf) {var newRecipes = Object.assign({}, recipes); return delete newRecipes.typeOf}
